b73de7952ee7fb039ca9d6ca6e6825a10ef12c6b
Ethan Guthrie <guthrieec@cofc.edu>
2020-03-02 19:39:54 -0500

    I am stuff

--- 340P2.py
+++ 340P2.py
@@ -15,17 +15,15 @@ def main():
         print("Usage:python3 340P2.py memorySize pageSize jobCount maxRunTime minRunTime maxMemory minMemory randomSeed")
         return -1
 
-    #how would you do the example he gives with set RANDOMSEED=13
-
     # Getting command line arguments.
-    memSize=int(argv[1])
-    pSize=int(argv[2])
-    jCount=int(argv[3])
-    maxRT=int(argv[4])
-    minRT=int(argv[5])
-    maxMem=int(argv[6])
-    minMem=int(argv[7])
-    seed=int(argv[8])
+    memSize = int(argv[1])
+    pSize = int(argv[2])
+    jCount = int(argv[3])
+    maxRT = int(argv[4])
+    minRT = int(argv[5])
+    maxMem = int(argv[6])
+    minMem = int(argv[7])
+    seed = int(argv[8])
 
     # Validating command line arguments and setting page count.
     if(memSize%pSize!=0):
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# 340_Project2
-Due March 3
+# CSCI340_Project2
+### Reid Foster, Ethan Guthrie
+
+## Usage
+Run the script using the following syntax:
+```
+python 340P2.py <memorySize> <pageSize> <jobCount> <maxRunTime> <minRunTime> <maxMemory> <minMemory> <randomSeed>
+```
+
+All parameters are assumed to be integers.
