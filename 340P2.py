7e8c5dbdd56ccdcf369e546be26c448a72572de5
Ethan Guthrie <guthrieec@cofc.edu>
2020-03-02 19:13:52 -0500

    I am stuff

--- 340P2.py
+++ 340P2.py
@@ -1,3 +1,7 @@
+# 340P2.py
+# Reid Foster and Ethan Guthrie
+# 03/03/2020
+# Simulates paging on an operating system with Python.
 
 # Libraries named after authors of this assignment contain methods written
 # by the author after whom the file was named.
@@ -11,8 +15,9 @@ def main():
         print("Usage:python3 340P2.py memorySize pageSize jobCount maxRunTime minRunTime maxMemory minMemory randomSeed")
         return -1
 
-        #how would you do the example he gives with set RANDOMSEED=13
+    #how would you do the example he gives with set RANDOMSEED=13
 
+    # Getting command line arguments.
     memSize=int(argv[1])
     pSize=int(argv[2])
     jCount=int(argv[3])
@@ -27,6 +32,7 @@ def main():
         return -1
     pageCount=memSize/pSize
 
+    # Printing output.
     jobs=createJobs(jCount,maxRT,minRT,maxMem,minMem,seed)
     printParameters(memSize,pSize,jCount,maxRT,minRT,maxMem,minMem,seed)
     printJobQueue(jobs)
--- ethan.py
+++ ethan.py
@@ -1,1 +1,6 @@
+# ethan.py
+# Ethan Guthrie
+# 03/03/2020
+# 
+
 from reid import *
